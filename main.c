3921873471c4b28b225e7eb43035203fe0574a24
MONNIN Adrian <Adrian.MONNIN@cpnv.ch>
2020-03-05 16:55:06 +0100

    work in progress

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello, World!\n");
+int main()
+{
+    int choix;
+    printf("1 - Afficher l'aide\n2- Commencer une partie\n3 - Afficher la liste des scores");
+    scanf("%d", &choix);
+    switch(choix)
     return 0;
 }
